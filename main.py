e302f53d8184e860ebc9ad1907195b495a7cca28
Davi <davimoreira58@gmail.com>
2024-07-26 09:47:24 -0300

    apagandos linhas vazias na tabela

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ db_file = os.path.join(source, 'cancelamentos.csv')
 
 table = pd.read_csv(db_file)
 
+table = table.drop(columns='CustomerID')
+
+print(table.info())
+
+table = table.dropna()
+
+print(table.info())
+
+
